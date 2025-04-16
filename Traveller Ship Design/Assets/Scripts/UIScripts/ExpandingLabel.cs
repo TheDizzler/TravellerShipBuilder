83c835252ad0b0184b6962565c7bf2ee93139722
DeckPuncher <tgillon.sdk@gmail.com>
2025-04-16 21:43:36 +0900

    improved expanding label

--- Traveller Ship Design/Assets/Scripts/UIScripts/ExpandingLabel.cs
+++ Traveller Ship Design/Assets/Scripts/UIScripts/ExpandingLabel.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class ExpandingLabel : MonoBehaviour
 {
-	[SerializeField] private float minLabelWidth = 1;
+	[SerializeField] private Vector2 minLabelDimensions = new Vector2(1, .5f);
+	[SerializeField] private Vector2 maxLabelDimensions = new Vector2(5, 2.5f);
 	[SerializeField] private TextMeshPro textLabel;
 	[SerializeField] private SpriteRenderer spriteLabel;
 
@@ -26,26 +27,42 @@ public class ExpandingLabel : MonoBehaviour
 	public Color color
 	{
 		get { return textLabel.color; }
-		set
-		{
-			textLabel.color = value;
-		}
+		set { textLabel.color = value; }
 	}
 
 	private void UpdateLabel()
 	{
-		var textWidth = textLabel.GetPreferredValues(_titleText).x;
-		var padding = textLabel.margin.x + textLabel.margin.z;
-		if (textWidth < minLabelWidth)
-			textWidth = minLabelWidth;
+		var prefTextSize = textLabel.GetPreferredValues(_titleText);
+		var rendTextSize = textLabel.GetRenderedValues();
+		var horzPadding = textLabel.margin.x + textLabel.margin.z;
+		var textWidth = prefTextSize.x;
+		var textHeight = prefTextSize.y; // this should be the preferred height of a single line, right?
 
-		textLabel.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, textWidth);
-		spriteLabel.size = new Vector2(textWidth + padding, spriteLabel.size.y);
+		float vertPadding = 0;
+		
+
+		if (textWidth < minLabelDimensions.x)
+			textWidth = minLabelDimensions.x;
+		if (textHeight < minLabelDimensions.y)
+			textHeight = minLabelDimensions.y;
+
+		var labelSize = new Vector2(textWidth + horzPadding, textHeight + vertPadding);
+		if (textWidth > maxLabelDimensions.x)
+		{
+			textWidth = maxLabelDimensions.x;
+			textHeight = rendTextSize.y;
+			labelSize.x = rendTextSize.x + horzPadding;
+			labelSize.y = textHeight + textLabel.margin.y + textLabel.margin.w;
+		}
+
+		var rect = textLabel.rectTransform;
+		rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, textWidth);
+		rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, textHeight);
+		spriteLabel.size = labelSize;
 	}
 
 	public void UpdateText()
 	{
 		text = _titleText;
-		UpdateLabel();
 	}
 }
